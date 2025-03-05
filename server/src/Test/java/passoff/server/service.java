e5cb9092f265360e19c71ca4d98f35a8479c2ea3
Bailey <bcalder3@byu.edu>
2025-03-04 21:00:16 -0700

    Chess Phase(3): Implemented a positive and negative test for login().

--- server/src/Test/java/passoff/server/service.java
+++ server/src/Test/java/passoff/server/service.java
@@ -1,11 +1,17 @@
 package passoff.server;
 
 import chess.ChessGame;
+import dataaccess.AuthDAOInterface;
+import dataaccess.MemoryAuthDAO;
 import dataaccess.MemoryUserDAO;
 import dataaccess.UserDAOInterface;
 import exceptionhandling.DataAccessException;
+import handlers.Handler;
+import model.AuthData;
 import model.UserData;
 import org.junit.jupiter.api.*;
+import requestresult.LoginRequest;
+import requestresult.LoginResult;
 import requestresult.RegisterRequest;
 import service.UserService;
 
@@ -13,16 +19,14 @@ import service.UserService;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class service {
     private static final UserDAOInterface userDAO = new MemoryUserDAO();
+    private static final AuthDAOInterface authDAO = new MemoryAuthDAO();
+
     @Test
     @Order(1)
     @DisplayName("Register: Success")
-    public void registerSuccess() {
+    public void registerSuccess() throws DataAccessException{
         RegisterRequest regReq = new RegisterRequest("Username", "Password", "EMAIL@GMAIL.COM");
-        try {
-            new UserService().register(regReq);
-        } catch (Exception e) {
-            System.out.printf(e.toString());
-        }
+        new UserService().register(regReq);
         UserData userData = userDAO.getUser("Username");
         Assertions.assertEquals(new UserData("Username", "Password", "EMAIL@GMAIL.COM"), userData,
                 "Registration was not correctly added to the database");
@@ -39,6 +43,30 @@ public class service {
         });
         Assertions.assertEquals("Error: already taken", exception.getDefaultMessage());
         Assertions.assertEquals(403, exception.getStatusCode());
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Login: Success")
+    public void loginSuccess() throws Exception{
+        userDAO.createUser(new UserData("Username", "Password", "email"));
+        LoginRequest logReq = new LoginRequest("Username", "Password");
+        LoginResult logRes = new UserService().login(logReq);
+        String authToken = logRes.authToken();
+        AuthData authData = authDAO.getAuth(authToken);
+        Assertions.assertEquals("Username", authData.username());
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Login: Unauthorized")
+    public void loginUnauthorized() {
+        LoginRequest logReq = new LoginRequest("Username", "Password");
+        DataAccessException exception = Assertions.assertThrows(DataAccessException.class, () -> {
+            new UserService().login(logReq);
+        });
+        Assertions.assertEquals("Error: unauthorized", exception.getDefaultMessage());
+        Assertions.assertEquals(401, exception.getStatusCode());
     }
 }
 
