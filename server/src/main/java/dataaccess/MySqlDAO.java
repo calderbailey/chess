9fd31ae85e9f6ed9697c4b08fcd7e634b2276b73
Bailey <bcalder3@byu.edu>
2025-03-15 11:00:55 -0600

    Chess Phase(4): Alterations to prevent deep nesting in the executeUpdate() method in MySqlDAO.

--- server/src/main/java/dataaccess/MySqlDAO.java
+++ server/src/main/java/dataaccess/MySqlDAO.java
@@ -27,27 +27,24 @@ public abstract class MySqlDAO {
     }
 
     protected int executeUpdate(String statement, Object... params) throws DataAccessException {
-        try (var conn = DatabaseManager.getConnection()) {
-            try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
-                for (var i = 0; i < params.length; i++) {
-                    var param = params[i];
-                    switch (param) {
-                        case String p -> ps.setString(i + 1, p);
-                        case Integer p -> ps.setInt(i + 1, p);
-                        case null -> ps.setNull(i + 1, NULL);
-                        default -> {
-                        }
-                    }
+        try (var conn = DatabaseManager.getConnection();
+             var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+            for (var i = 0; i < params.length; i++) {
+                var param = params[i];
+                switch (param) {
+                    case String p -> ps.setString(i + 1, p);
+                    case Integer p -> ps.setInt(i + 1, p);
+                    case null -> ps.setNull(i + 1, NULL);
+                    default -> { }
                 }
-                ps.executeUpdate();
-
-                var rs = ps.getGeneratedKeys();
+            }
+            ps.executeUpdate();
+            try (var rs = ps.getGeneratedKeys()) {
                 if (rs.next()) {
                     return rs.getInt(1);
                 }
-
-                return 0;
             }
+            return 0;
         } catch (SQLException e) {
             throw new DataAccessException(String.format("unable to update database: %s, %s", statement, e.getMessage()), 500);
         }
